393569f0afb02ecd4403543e2670d872377e1842
Josh <68549085+J-716@users.noreply.github.com>
2025-05-01 01:18:06 -0400

    Maybe this works

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,4 @@
 # Disable greeting
 set fish_greeting
 
-function gitw
-    git -c user.signingkey=$HOME/.ssh/id_ed25519_work $argv
-end
-
-function gitp
-    git -c user.signingkey=$HOME/.ssh/id_ed25519_personal $argv
-end
 
