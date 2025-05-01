8d1f278ea0fa4f2dda5b2ba8fe8005b8e74c5673
Josh <68549085+J-716@users.noreply.github.com>
2025-05-01 00:50:58 -0400

    Maybe this works

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,13 @@
 # Disable greeting
 set fish_greeting
+
+direnv hook fish | source
+
+# Git commit wrappers to inject signing keys
+function gitw
+    git -c user.signingkey=$HOME/.ssh/id_ed25519_work $argv
+end
+
+function gitp
+    git -c user.signingkey=$HOME/.ssh/id_ed25519_personal $argv
+end
